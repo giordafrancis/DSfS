ef5613a8e8a4b6798e5484bb4180193ebb3cb046
Francisco G Silva <giordanofrancisco@hotmail.com>
2019-07-26 17:21:33 +0100

    added gradient_descent

--- linear_algebra.py
+++ linear_algebra.py
@@ -22,7 +22,6 @@ def add(v: Vector, w: Vector) -> Vector:
     """Adds corresponding elements"""
     assert len(v) == len(w), "vectors must be the same length"
     return [v_i + w_i for v_i, w_i in zip(v, w)]
-    
 
 
 assert add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
